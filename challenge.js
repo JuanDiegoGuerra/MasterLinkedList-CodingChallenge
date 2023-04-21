17b34781b37a9fe48fa2a7f4662a3891f0e6a138
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-21 16:28:30 -0500

    Create challenge.js

---
+++ challenge.js
@@ -0,0 +1,15 @@
+
+const list = new LinkedList()
+
+list.add(3)
+list.add(5)
+list.addAt(1, 11)
+list.addAt(0, 13)
+
+console.log(list.get(2))
+// => 11
+
+console.log(list.get(3))
+// => 5
+
+module.exports = LinkedList
