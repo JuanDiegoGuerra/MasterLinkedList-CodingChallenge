e6c1a7f6aac9f53a7f7b26450a414240d90f216f
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-21 16:39:12 -0500

    Coding Challenge Solution
    Final Solution Linked List Next Step Coding Challenge

--- challenge.js
+++ challenge.js
@@ -1,32 +1,104 @@
 // Start with your code from last challenge.
 class Node {
-    constructor(value, next_node = null) {
-      this.value = value;
-      this.next_node = next_node;
+  constructor(value, next_node = null) {
+    this.value = value;
+    this.next_node = next_node;
+  }
+}
+
+class LinkedList {
+  // setup head and tail
+  constructor(){
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  add(number) {
+    // your code here
+    const node = new Node(number);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next_node = node;
+      this.tail = node;
+    }
+    this.length++;   
+    }
+
+    getLast() {
+      if (!this.head) {
+        return null;
+      }
+      let node = this.head;
+      while (node) {
+       if (!node.next_node) {
+        return node;
+       }
+       node = node.next_node;
+      }
+    }
+
+    getAt(index) {
+      let counter = 0;
+      let node = this.head;
+      while (node) {
+       if (counter === index) {
+        return node;
+       }
+       counter++;
+       node = node.next_node;
+      }
+     return null;
+    }
+
+  addAt(index, element) {
+      if (!this.head) {
+          this.head = new Node(element);
+          return;
+        }
+    
+        if (index === 0) {
+          this.head = new Node(element, this.head);
+          return;
+        }
+        const previous = this.getAt(index - 1) || this.getLast();
+        const node = new Node(element, previous.next_node);
+        previous.next_node = node;
+  }
+
+  remove(index) {
+      if (!this.head) {
+        return;
+      }
+      if (index === 0) {
+        this.head = this.head.next_node;
+        return;
+      }
+      
+      const previous = this.getAt(index - 1);
+      if (!previous || !previous.next_node) {
+        return;
+      }
+      previous.next_node = previous.next_node.next_node;
+     }
+  
+
+  get(index) {
+    // your code here
+    let counter = 0;
+    let currentNode  = this.head;
+    while (currentNode ) {
+     if (counter === index) {
+      return currentNode.value;
+     }
+     counter++;
+     currentNode  = currentNode.next_node;
+    }
+   return currentNode;
     }
   }
-  
-  class LinkedList {
-  
-    add(number) {
-      }
-
-      getLast() {
-      }
-
-      getAt(index) {
-      }
-
-    addAt(index, element) {
-    }
-
-    remove(index) {
-       }
-    
-  
-    get(index) {
-      }
-    }
 
 const list = new LinkedList()
 
